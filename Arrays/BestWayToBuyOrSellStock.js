a8f07d5ff059aa65cf2978380f538eee1f132590
ananyadhananjaya <ananya.dhananjaya1998@gmail.com>
2022-07-09 21:15:43 +0530

    feat: error correction

--- Arrays/BestWayToBuyOrSellStock.js
+++ Arrays/BestWayToBuyOrSellStock.js
@@ -1,15 +1,14 @@
-let prices = [7,1,2,3,5,6,1,10]
+let prices = [7, 1, 2, 3, 5, 6, 1, 10, 1];
 
-let profit = 0
+let profit = 0;
 
-let minS = 0
+let minS = 0;
 
-for(let i= 1; i< prices.length;i++){
-    if(prices[i]< prices[minS]){
-        minS = prices[i]
-        
-    }
-    profit =   prices[i] -prices[minS]
+for (let i = 1; i < prices.length; i++) {
+  if (prices[i] < prices[minS]) {
+    minS = prices[i];
+  }
+  profit = Math.max(prices[i] - prices[minS], profit);
 }
 
-console.log("Day to buy stock", minS, "we get", profit, "profit")
+console.log("Day to buy stock ", minS, " we get ", profit);
