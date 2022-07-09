290ebdf75692c990e81151904e797452f4b28603
ananyadhananjaya <ananya.dhananjaya1998@gmail.com>
2022-07-09 17:55:56 +0530

    feat: Tag for blind 75

--- Arrays/TwoSum.js
+++ Arrays/TwoSum.js
@@ -1,13 +1,15 @@
-let arr = [2, 7, 11, 15];
-let target = 9;
+let arr = [1,2,3,4]
+let target = 5
 
-let dict = {};
+let dict = {}
+let total = arr.reduce((a,b)=> a+b)
 
-for (let i = 0; i < arr.length; i++) {
-  let diff = target - arr[i];
-  if (dict[diff] !== undefined) {
-    console.log(dict[diff], i);
-  } else {
-    dict[arr[i]] = i;
+for(let i=0; i<arr.length; i++){
+  let diff = target - arr[i]
+  if(dict[diff]!== undefined){
+    console.log(dict[diff], i)
+  }
+  else{
+    dict[arr[i]] = i
   }
 }
