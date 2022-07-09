f7c5626f5285d2bcf86c0258d461922bc76c8f59
ananyadhananjaya <ananya.dhananjaya1998@gmail.com>
2022-07-09 21:10:02 +0530

    chore: format

--- Arrays/TwoSum.js
+++ Arrays/TwoSum.js
@@ -1,15 +1,15 @@
-let arr = [1,2,3,4]
-let target = 5
+let arr = [2, 7, 11, 15];
+let target = 9;
 
-let dict = {}
-let total = arr.reduce((a,b)=> a+b)
+let dict = {};
 
-for(let i=0; i<arr.length; i++){
-  let diff = target - arr[i]
-  if(dict[diff]!== undefined){
-    console.log(dict[diff], i)
-  }
-  else{
-    dict[arr[i]] = i
+for (let i = 0; i < arr.length; i++) {
+  let diff = target - arr[i];
+  if (dict[diff] !== undefined) {
+    console.log(dict[diff], i);
+  } else {
+    dict[arr[i]] = i;
   }
 }
+
+//Blind 75
