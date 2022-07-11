73019888b42ffc6f3cd9abbe23906e88746af066
ananyadhananjaya <ananya.dhananjaya1998@gmail.com>
2022-07-11 20:24:43 +0530

    feat: Minimum In Rotated Sorted Array

---
+++ Arrays/MinimumInRotatedSortedArray.js
@@ -0,0 +1,18 @@
+
+let nums = [7, 8, 9, 3, 4];
+
+let start = 0, end = nums.length-1;
+
+while(start < end){
+    if(nums[start] < nums[end]){
+        console.log("Min is:", nums[start])
+        break;
+    }
+
+    let mid = Math.floor(start+end/2)
+    if(nums[mid]>= nums[start]){
+        start = mid+1
+    }else{
+        end = mid
+    }
+}
